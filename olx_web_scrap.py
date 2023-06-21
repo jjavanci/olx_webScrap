49fe69e7a4df3fcf99b421f2471eac8999550a71
Juliano Avanci da Silva <jjdevnet01@gmail.com>
2023-06-21 07:45:22 -0300

    Coletando todas as páginas

--- olx_web_scrap.py
+++ olx_web_scrap.py
@@ -11,12 +11,12 @@ site = requests.get(url, headers=headers)
 soup = BeautifulSoup(site.content, 'html.parser')
 carros = soup.find_all('div', class_='hrShZb')
 
+#ultima_pagina = soup.find_all('span', class_='sc-1bofr6e-1 hEtrnt sc-ifAKCX bVUFRk')[-1]
+
 i = 0
 
 for carro in carros:
 
-   
-    
     try:
         link_carro = carro.find('a', class_='gIhjul')
         nome_carro = carro.find('h2', class_='hUnWqk').get_text()
@@ -36,7 +36,7 @@ for carro in carros:
         print("IndexError")
 
     i += 1
-
+    print(url)
     print(link_carro.get('href'))
     print()
     print(nome_carro)
@@ -51,3 +51,49 @@ for carro in carros:
     print('####################################################################################################################')
 
 print(str(i) + ' anúncios')
+print('FIM PÁGINA 1')
+
+
+for i in range(2, 101):
+    
+    url = f'https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-sp?o={i}'
+    print(url)
+    site = requests.get(url, headers=headers)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    carros = soup.find_all('div', class_='hrShZb')
+    
+    for carro in carros:
+
+        try:
+            link_carro = carro.find('a', class_='gIhjul')
+            nome_carro = carro.find('h2', class_='hUnWqk').get_text()
+            preco_carro = carro.find('h3', class_='bytyxL').get_text()#[3:]
+            info_carro = carro.findAll('span')[0].get_text()
+            info_carro1 = carro.findAll('span')[1].get_text()
+            info_carro2 = carro.findAll('span')[2].get_text()
+            info_carro3 = carro.findAll('span')[3].get_text()
+    
+        except:
+            nome_carro = 'n/c'
+            preco_carro = 'n/c'
+            info_carro = "n/c"
+            info_carro1 = "n/c"
+            info_carro2 = "n/c"
+            info_carro3 = "n/c"
+            print("IndexError")
+
+        print(url)
+        print(link_carro.get('href'))
+        print()
+        print(nome_carro)
+        print()
+        print(preco_carro)
+        print()
+        print(info_carro)
+        print(info_carro1)
+        print(info_carro2)
+        print(info_carro3)
+        print()
+        print('####################################################################################################################')
+    
+    print('####Página ' + str(i))
